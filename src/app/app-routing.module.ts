1160dc699df9713657c2116bc050c738297ac1ce
Joe Monkila <jnkiwa25@gmail.com>
2023-11-09 15:32:37 +0100

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './presentation/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', pathMatch: 'full', redirectTo: 'dashboard'},
+  {path:'dashboard', component:DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
