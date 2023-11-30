724174769c67c3c63c91992bb77e7344e9835919
Joe Monkila <jnkiwa25@gmail.com>
2023-11-30 11:18:07 +0100

    model, repositories and usecase

---
+++ src/app/data/repositories/user/user-implementation.repository.ts
@@ -0,0 +1,34 @@
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { UserEntity } from './entities/user-entity';
+import { UserImplementationRepositoryMapper } from './mappers/user-repository.mapper';
+import { UserRepository } from '../../../domain/repositories/user.repository';
+import { UserModel } from '../../../domain/models/user.model';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class UserImplementationRepository extends UserRepository {
+    userMapper = new UserImplementationRepositoryMapper();
+
+    constructor(private http: HttpClient) {
+        super();
+    }
+
+    login(params: {username: string, password: string}): Observable<UserModel> {
+        return this.http
+            .post<UserEntity>('https://example.com/login', {params})
+            .pipe(map(this.userMapper.mapFrom));
+    }
+    register(params: {username: string, password: string}): Observable<UserModel> {
+       return this.http
+            .post<UserEntity>('https://example.com/register', {params})
+            .pipe(map(this.userMapper.mapFrom));
+    }
+    getUserProfile(): Observable<UserModel>{
+        return this.http.get<UserEntity>('https://example.com/user').pipe(
+            map(this.userMapper.mapFrom));
+    }
+}
