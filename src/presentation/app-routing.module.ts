e91ad8734339269fa0a51a105a38973c50f552ee
Joe Monkila <jnkiwa25@gmail.com>
2023-11-30 11:48:00 +0100

    update ui

--- src/presentation/app-routing.module.ts
+++ src/presentation/app-routing.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './presentation/dashboard/dashboard.component';
-import { GestionEnseignantComponent } from './presentation/dashboard/pages/gestion-enseignant/gestion-enseignant.component';
-import { PresenceEnseignantComponent } from './presentation/dashboard/pages/gestion-enseignant/presence-enseignant/presence-enseignant.component';
-import { PayeEnseignantComponent } from './presentation/dashboard/pages/gestion-enseignant/paye-enseignant/paye-enseignant.component';
-import { FinanceEnseignantComponent } from './presentation/dashboard/pages/gestion-enseignant/finance-enseignant/finance-enseignant.component';
-import { ImpressionEnseignantComponent } from './presentation/dashboard/pages/gestion-enseignant/impression-enseignant/impression-enseignant.component';
-import { GestionElevesComponent } from './presentation/dashboard/pages/gestion-eleves/gestion-eleves.component';
-import { FinanceElevesComponent } from './presentation/dashboard/pages/gestion-eleves/finance-eleves/finance-eleves.component';
-import { ImpressionElevesComponent } from './presentation/dashboard/pages/gestion-eleves/impression-eleves/impression-eleves.component';
-import { GestionOuvrierComponent } from './presentation/dashboard/pages/gestion-ouvrier/gestion-ouvrier.component';
-import { PresenceOuvrierComponent } from './presentation/dashboard/pages/gestion-ouvrier/presence-ouvrier/presence-ouvrier.component';
-import { PayeOuvrierComponent } from './presentation/dashboard/pages/gestion-ouvrier/paye-ouvrier/paye-ouvrier.component';
-import { FinanceOuvrierComponent } from './presentation/dashboard/pages/gestion-ouvrier/finance-ouvrier/finance-ouvrier.component';
-import { ImpressionOuvrierComponent } from './presentation/dashboard/pages/gestion-ouvrier/impression-ouvrier/impression-ouvrier.component';
-import { ParametreComponent } from './presentation/dashboard/pages/parametre/parametre.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GestionEnseignantComponent } from './dashboard/pages/gestion-enseignant/gestion-enseignant.component';
+import { PresenceEnseignantComponent } from './dashboard/pages/gestion-enseignant/presence-enseignant/presence-enseignant.component';
+import { PayeEnseignantComponent } from './dashboard/pages/gestion-enseignant/paye-enseignant/paye-enseignant.component';
+import { FinanceEnseignantComponent } from './dashboard/pages/gestion-enseignant/finance-enseignant/finance-enseignant.component';
+import { ImpressionEnseignantComponent } from './dashboard/pages/gestion-enseignant/impression-enseignant/impression-enseignant.component';
+import { GestionElevesComponent } from './dashboard/pages/gestion-eleves/gestion-eleves.component';
+import { FinanceElevesComponent } from './dashboard/pages/gestion-eleves/finance-eleves/finance-eleves.component';
+import { ImpressionElevesComponent } from './dashboard/pages/gestion-eleves/impression-eleves/impression-eleves.component';
+import { GestionOuvrierComponent } from './dashboard/pages/gestion-ouvrier/gestion-ouvrier.component';
+import { PresenceOuvrierComponent } from './dashboard/pages/gestion-ouvrier/presence-ouvrier/presence-ouvrier.component';
+import { PayeOuvrierComponent } from './dashboard/pages/gestion-ouvrier/paye-ouvrier/paye-ouvrier.component';
+import { FinanceOuvrierComponent } from './dashboard/pages/gestion-ouvrier/finance-ouvrier/finance-ouvrier.component';
+import { ImpressionOuvrierComponent } from './dashboard/pages/gestion-ouvrier/impression-ouvrier/impression-ouvrier.component';
+import { ParametreComponent } from './dashboard/pages/parametre/parametre.component';
 
 const routes: Routes = [
   {path:'', pathMatch: 'full', redirectTo: 'dashboard'},
--- src/presentation/app.module.ts
+++ src/presentation/app.module.ts
@@ -3,21 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './presentation/dashboard/dashboard.component';
-import { GestionEnseignantComponent } from './presentation/dashboard/pages/gestion-enseignant/gestion-enseignant.component';
-import { PresenceEnseignantComponent } from './presentation/dashboard/pages/gestion-enseignant/presence-enseignant/presence-enseignant.component';
-import { FinanceEnseignantComponent } from './presentation/dashboard/pages/gestion-enseignant/finance-enseignant/finance-enseignant.component';
-import { PayeEnseignantComponent } from './presentation/dashboard/pages/gestion-enseignant/paye-enseignant/paye-enseignant.component';
-import { ImpressionEnseignantComponent } from './presentation/dashboard/pages/gestion-enseignant/impression-enseignant/impression-enseignant.component';
-import { GestionElevesComponent } from './presentation/dashboard/pages/gestion-eleves/gestion-eleves.component';
-import { FinanceElevesComponent } from './presentation/dashboard/pages/gestion-eleves/finance-eleves/finance-eleves.component';
-import { ImpressionElevesComponent } from './presentation/dashboard/pages/gestion-eleves/impression-eleves/impression-eleves.component';
-import { GestionOuvrierComponent } from './presentation/dashboard/pages/gestion-ouvrier/gestion-ouvrier.component';
-import { PresenceOuvrierComponent } from './presentation/dashboard/pages/gestion-ouvrier/presence-ouvrier/presence-ouvrier.component';
-import { FinanceOuvrierComponent } from './presentation/dashboard/pages/gestion-ouvrier/finance-ouvrier/finance-ouvrier.component';
-import { PayeOuvrierComponent } from './presentation/dashboard/pages/gestion-ouvrier/paye-ouvrier/paye-ouvrier.component';
-import { ImpressionOuvrierComponent } from './presentation/dashboard/pages/gestion-ouvrier/impression-ouvrier/impression-ouvrier.component';
-import { ParametreComponent } from './presentation/dashboard/pages/parametre/parametre.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GestionEnseignantComponent } from './dashboard/pages/gestion-enseignant/gestion-enseignant.component';
+import { PresenceEnseignantComponent } from './dashboard/pages/gestion-enseignant/presence-enseignant/presence-enseignant.component';
+import { FinanceEnseignantComponent } from './dashboard/pages/gestion-enseignant/finance-enseignant/finance-enseignant.component';
+import { PayeEnseignantComponent } from './dashboard/pages/gestion-enseignant/paye-enseignant/paye-enseignant.component';
+import { ImpressionEnseignantComponent } from './dashboard/pages/gestion-enseignant/impression-enseignant/impression-enseignant.component';
+import { GestionElevesComponent } from './dashboard/pages/gestion-eleves/gestion-eleves.component';
+import { FinanceElevesComponent } from './dashboard/pages/gestion-eleves/finance-eleves/finance-eleves.component';
+import { ImpressionElevesComponent } from './dashboard/pages/gestion-eleves/impression-eleves/impression-eleves.component';
+import { GestionOuvrierComponent } from './dashboard/pages/gestion-ouvrier/gestion-ouvrier.component';
+import { PresenceOuvrierComponent } from './dashboard/pages/gestion-ouvrier/presence-ouvrier/presence-ouvrier.component';
+import { FinanceOuvrierComponent } from './dashboard/pages/gestion-ouvrier/finance-ouvrier/finance-ouvrier.component';
+import { PayeOuvrierComponent } from './dashboard/pages/gestion-ouvrier/paye-ouvrier/paye-ouvrier.component';
+import { ImpressionOuvrierComponent } from './dashboard/pages/gestion-ouvrier/impression-ouvrier/impression-ouvrier.component';
+import { ParametreComponent } from './dashboard/pages/parametre/parametre.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
