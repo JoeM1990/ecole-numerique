1868e9ff212993419caa5914d476a74464d1668e
Joe Monkila <jnkiwa25@gmail.com>
2023-12-15 13:32:42 +0100

    dialog

--- src/presentation/auth-ui/login/login.component.ts
+++ src/presentation/auth-ui/login/login.component.ts
@@ -1,7 +1,9 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { UserLoginUseCase } from 'src/domain/usecases/user-login.usecase';
+import { ErrorComponent } from 'src/presentation/dialog/error/error.component';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +14,7 @@ export class LoginComponent implements OnInit{
 
   state=false;
 
-  constructor(private router:Router, private auth:UserLoginUseCase){
+  constructor(private router:Router, private auth:UserLoginUseCase, private dialog:MatDialog){
 
   }
   
@@ -29,17 +31,17 @@ export class LoginComponent implements OnInit{
     this.state = true;
 
     if(!email){
-      console.log('Veuillez introduire le nom d utilisateur');
+      this.dialog.open(ErrorComponent,{data:"Veuillez introduire le nom d'utilisateur"});
       this.state = false;
     }else if(!password){
-      console.log('Veuillez introduire le mot de passe');
+      this.dialog.open(ErrorComponent,{data:'Veuillez introduire le mot de passe'});
       this.state = false;
     }else{
       this.auth.execute({email,password}).subscribe((res)=>{
         console.log(res.accessToken);
         this.state = false;
           }, (err:HttpErrorResponse) => {
-            console.log(err.error.message);
+            this.dialog.open(ErrorComponent,{data:err.error.message});
             this.state = false;
         }
       )
--- src/presentation/auth-ui/register/register.component.ts
+++ src/presentation/auth-ui/register/register.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserRegisterUseCase } from 'src/domain/usecases/user-register.usecase';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorComponent } from 'src/presentation/dialog/error/error.component';
 
 
 @Component({
@@ -16,7 +18,7 @@ export class RegisterComponent implements OnInit{
   state = false;
   
   constructor(private router:Router, private auth:UserRegisterUseCase,
-    public formBuilder: FormBuilder){
+    public formBuilder: FormBuilder, private dialog:MatDialog){
       this.registerForm = this.formBuilder.group({
         username: ['', [Validators.required], ],
         email: ['', [Validators.required], ],
@@ -38,11 +40,11 @@ export class RegisterComponent implements OnInit{
     this.state = true;
 
     this.auth.execute(this.registerForm.value).subscribe((res)=>{
-        console.log('Votre compte a été créé avec succès');
+        this.dialog.open(ErrorComponent,{data:'Votre compte a été créé avec succès'})
         this.state = false;
         this.router.navigate(['/login'])
       }, (err:HttpErrorResponse) => {
-        console.log(err.error.message)
+        this.dialog.open(ErrorComponent,{data:err.error.message})
         this.state = false;
      }
     )
