b38e119ed95deaaa50e35c7cc0e911afc6574f0b
Joe Monkila <jnkiwa25@gmail.com>
2023-12-16 09:59:47 +0100

    guard

--- src/domain/usecases/auth.ts
+++ src/domain/usecases/auth.ts
@@ -1,18 +1,29 @@
+import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 
+@Injectable({
+    providedIn: 'root'
+  })
 export class Auth {
+
+    private isAuthenticated = false;
 
     constructor(private router:Router) { }
 
-    checkLogin(){
+    checkLogin():boolean{
+        this.isAuthenticated = true;
         return !! localStorage.getItem('token');
+    }
+
+      isAuthenticatedUser(): boolean {
+        return this.isAuthenticated;
       }
   
   
     logout(){
-        localStorage.removeItem('token')
+        localStorage.removeItem('token');
+        this.isAuthenticated = false;
         this.router.navigate(['/login']);
-        return 'dk';
       }
 
 
